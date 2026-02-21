2e18fba35c718a81b88a13211d65b9d68c9c4fab
Timofey Votyakov <tvotyakov@gmail.com>
2026-02-21 19:49:47 +0100

    Adds QA Theory part

---
+++ QA Theory/QA Theory.mm
@@ -0,0 +1,338 @@
+<map version="freeplane 1.12.15">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<bookmarks/>
+<node TEXT="QA Theory" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1771428651033" VGAP_QUANTITY="3 pt"><hook NAME="MapStyle" background="#d6e8e8ff" zoom="0.826">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" auto_compact_layout="true" show_tags="UNDER_NODES" show_note_icons="true" associatedTemplateLocation="template:/light_sky_element_template.mm" fit_to_viewport="false" show_icons="BESIDE_NODES" showTagCategories="false"/>
+    <tags category_separator="::"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_4172522" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#051552" BACKGROUND_COLOR="#5cd5e8" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.7 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#1164b0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID" VGAP_QUANTITY="3 pt">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_4172522" STARTINCLINATION="115.8 pt;0 pt;" ENDINCLINATION="115.8 pt;28.2 pt;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#051552" WIDTH="2" DASH="SOLID"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" COLOR="#fff024" BACKGROUND_COLOR="#000000"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.tags">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#f6f9a1" TEXT_ALIGN="LEFT">
+<icon BUILTIN="clock2"/>
+<font SIZE="10" ITALIC="true"/>
+<edge COLOR="#000000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#ffffff" BACKGROUND_COLOR="#cc7212" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#1164b0"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_1823054225" COLOR="#fff024" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#9d5e19">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#9d5e19" TRANSPARENCY="255" DESTINATION="ID_1823054225"/>
+<font SIZE="11" BOLD="true"/>
+<edge COLOR="#9d5e19"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS" MAX_WIDTH="6 cm" MIN_WIDTH="3 cm"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#053d8b" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#2c2b29" BORDER_DASH_LIKE_EDGE="true">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#ffffff" BACKGROUND_COLOR="#1164b0" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR="#2c2b29">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#ffffff" BACKGROUND_COLOR="#298bc8" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" VGAP_QUANTITY="3 pt">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#ffffff" BACKGROUND_COLOR="#3fb7db" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#051552" BACKGROUND_COLOR="#5cd5e8" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BORDER_COLOR="#f0f0f0">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11" BORDER_COLOR="#f0f0f0">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font BOLD="true"/>
+<richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <span style="color: #f2f2f2; font-weight: bold;">Тестирование (Software Testing)</span><span style="color: #f2f2f2;">&#xa0;&#xa0;- проверка соответстия между </span><span style="color: #6666ff; font-weight: bold;">реальным (actual)</span><span style="color: #f2f2f2;">&#xa0;и </span><span style="color: #6666ff; font-weight: bold;">ожидаемым (expected)</span><span style="color: #f2f2f2;">&#xa0;поведением приложения.</span>
+    </p>
+  </body>
+</html></richcontent>
+<node TEXT=" Зачем?" POSITION="bottom_or_right" ID="ID_1248201643" CREATED="1771107464782" MODIFIED="1771107474564">
+<node TEXT="Ошибки дорого исправлять, после релиза" ID="ID_1387108658" CREATED="1771107474577" MODIFIED="1771107487842"/>
+<node TEXT="Недопонимание по требованиям" ID="ID_343533350" CREATED="1771107490048" MODIFIED="1771107564416"/>
+<node TEXT="Пользователи ожидают стабильную и удобную работу" ID="ID_155216490" CREATED="1771107516179" MODIFIED="1771107557934"/>
+</node>
+<node TEXT="Направления" POSITION="top_or_left" ID="ID_841836511" CREATED="1771428538331" MODIFIED="1771428543605">
+<node TEXT="Функциональное тестирование" ID="ID_476140700" CREATED="1771428543614" MODIFIED="1771428840036"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <span style="color: #ffffff;">Тестирование (обычное мануальное) функционала приложения, его UI</span>
+    </p>
+  </body>
+</html></richcontent>
+<node TEXT="Software Tester" ID="ID_926494061" CREATED="1771428709653" MODIFIED="1771428715242"/>
+<node TEXT="Software Test Analyst" ID="ID_110756484" CREATED="1771428715921" MODIFIED="1771428724373"/>
+<node TEXT="UI/UX Tester" ID="ID_434680567" CREATED="1771428727140" MODIFIED="1771428730933"/>
+</node>
+<node TEXT="Автоматизированное тестирование" ID="ID_1544444589" CREATED="1771428733293" MODIFIED="1771428742024">
+<node TEXT="Automation Test Engineer" ID="ID_1278186141" CREATED="1771428744972" MODIFIED="1771428755452"/>
+<node TEXT="SDET (Software Development in Test)" ID="ID_474188002" CREATED="1771428756010" MODIFIED="1771428773622"/>
+</node>
+<node TEXT="Нефункциональное тестирование" ID="ID_1099287089" CREATED="1771428781243" MODIFIED="1771428791914">
+<node TEXT="Load Testing Engineer" ID="ID_142178262" CREATED="1771428823729" MODIFIED="1771428857193"/>
+<node TEXT="Performance Tester" ID="ID_347460345" CREATED="1771428859601" MODIFIED="1771428865563"/>
+<node TEXT="Security Tester" ID="ID_1232031664" CREATED="1771428866031" MODIFIED="1771428871103"/>
+</node>
+<node TEXT="Тестирование баз данных" ID="ID_1260398173" CREATED="1771428872821" MODIFIED="1771428882455">
+<node TEXT="Database QA Engineer" ID="ID_698522841" CREATED="1771428883106" MODIFIED="1771428893306"/>
+<node TEXT="Data Quality Analyst" ID="ID_1125734731" CREATED="1771428893811" MODIFIED="1771428903646"/>
+</node>
+<node TEXT="Мобильное тестирование" ID="ID_423837253" CREATED="1771428904652" MODIFIED="1771428914184">
+<node TEXT="Mobile QA Engineer" ID="ID_1942913332" CREATED="1771428914774" MODIFIED="1771428928763"/>
+</node>
+<node TEXT="Тестирование API" ID="ID_241249958" CREATED="1771428930211" MODIFIED="1771428939794">
+<node TEXT="API Tester" ID="ID_822589486" CREATED="1771428940274" MODIFIED="1771428944185"/>
+<node TEXT="API Automation Engineer" ID="ID_1676701447" CREATED="1771428945691" MODIFIED="1771428953745"/>
+</node>
+<node TEXT="Тестирование игр" ID="ID_135854465" CREATED="1771428955052" MODIFIED="1771428964556">
+<node TEXT="GameDev tester" ID="ID_1365435801" CREATED="1771428964571" MODIFIED="1771428972885"/>
+</node>
+<node TEXT="Тестирование AI и ML" ID="ID_62583166" CREATED="1771428974013" MODIFIED="1771428989895">
+<node TEXT="AI Tester" ID="ID_769701885" CREATED="1771428989905" MODIFIED="1771428993865"/>
+<node TEXT="ML Testing Engineer" ID="ID_734968214" CREATED="1771428994293" MODIFIED="1771428999816"/>
+</node>
+</node>
+<node TEXT="Цели тестирования (по ISTQB)" POSITION="bottom_or_right" ID="ID_1637650696" CREATED="1771429035752" MODIFIED="1771429052495">
+<node TEXT="Предотвращение появления дефектов" ID="ID_472653431" CREATED="1771429053506" MODIFIED="1771429077867"/>
+<node TEXT="Верификация требований - Verification" ID="ID_1256545779" CREATED="1771429078654" MODIFIED="1771429093997">
+<node TEXT="проверить, что обозначенные требования реализованы" ID="ID_151259719" CREATED="1771429223807" MODIFIED="1771429232977"/>
+</node>
+<node TEXT="Валидация ожиданий заказчика - Validation" ID="ID_1039637510" CREATED="1771429094454" MODIFIED="1771429108146">
+<node TEXT="проверка соответвие неформальным ожиданиям: удобство пользования и т.п." ID="ID_1896050246" CREATED="1771429234387" MODIFIED="1771429279879"/>
+</node>
+<node TEXT="Создание уверенности в тестируемом продукте" ID="ID_653720510" CREATED="1771429111564" MODIFIED="1771429124346"/>
+<node TEXT="Снижение рисков путём нахождения дефектов" ID="ID_1659659297" CREATED="1771429124704" MODIFIED="1771429138238"/>
+<node TEXT="Предоставление информации для принятия решений (for Stakeholders)" ID="ID_1022179500" CREATED="1771429138674" MODIFIED="1771429166867"/>
+<node TEXT="Соответствие формальным требованиям (регуляторы, стандарты, законы)" ID="ID_1826279493" CREATED="1771429167535" MODIFIED="1771429185798"/>
+</node>
+<node TEXT="Пирамида тестирования" POSITION="top_or_left" ID="ID_1487697269" CREATED="1771429886893" MODIFIED="1771430155673" VGAP_QUANTITY="3 pt">
+<node TEXT="QA - Quality Assurance - обеспечение качества разработки" ID="ID_26890839" CREATED="1771429897744" MODIFIED="1771430803721">
+<arrowlink DESTINATION="ID_1206920087" STARTINCLINATION="115.8 pt;0 pt;" ENDINCLINATION="119.4 pt;0 pt;"/>
+<richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <span style="color: #ffffff;">Позволяет повысить уверенность в качестве конечного продукта. Включает в себя: </span>
+    </p>
+    <ul>
+      <li>
+        <span style="color: #ffffff;">планирование </span>
+      </li>
+      <li>
+        <span style="color: #ffffff;">проектирование </span>
+      </li>
+      <li>
+        <span style="color: #ffffff;">реализацию стратегий и процессов QA</span>
+      </li>
+    </ul>
+    <p>
+      <span style="color: #ffffff; font-weight: bold;">Задача: </span><span style="color: #ffffff; font-weight: normal;">предотвращение дефектов, обеспечение соответствия требованиям, повышение качества</span>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="QC - Quality Control - контроль качества продукта" ID="ID_1206920087" CREATED="1771429925754" MODIFIED="1771430971919">
+<arrowlink DESTINATION="ID_127979612" STARTINCLINATION="115.8 pt;0 pt;" ENDINCLINATION="153.6 pt;4.8 pt;"/>
+<richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <span style="color: #ffffff;">Выявление и исправление дефектов в готовом продукте. </span>
+    </p>
+    <p>
+      <span style="font-weight: bold; color: #ffffff;">Задача: </span><span style="font-weight: normal; color: #ffffff;">предоставлять информации о текущем качестве ПО на всех этапах разработки. </span>
+    </p>
+    <p>
+      <span style="font-weight: normal; color: #ffffff;">Включает в себя проверку </span><span style="font-weight: bold; color: #ffffff;">конкретных компонентов, кода и процессов</span><span style="font-weight: normal; color: #ffffff;">, чтобы убедиться, что они соответствуют установленным стандартам качества.</span>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="Testing - тестирование" ID="ID_127979612" CREATED="1771429950253" MODIFIED="1771431040351"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <span style="color: #ffffff;">Процесс, помогающий определить корректность, полноту и качество разрабатываемого ПО. В тестировании используются методы, инструменты и техники обнаружения и исправления дефектов.</span>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node TEXT="Основные понятия" POSITION="bottom_or_right" ID="ID_275069420" CREATED="1771432159411" MODIFIED="1771432165663">
+<node TEXT="Ошибка (Error)" ID="ID_1034118502" CREATED="1771432165675" MODIFIED="1771432443679"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="vertical-align: top; font-weight: normal; font-style: normal; color: #ffffff">
+      действие человека, приводящее к неверному результату.
+    </p>
+    <p style="vertical-align: top; font-weight: normal; font-style: normal; color: #ffffff">
+      E.g.: программист допустил опечатку в коде.
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node ID="ID_1764461737" CREATED="1771432451152" MODIFIED="1771432551498"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Дефект (Defect or <span style="font-weight: bold;">Bug</span>)
+    </p>
+  </body>
+</html>
+</richcontent>
+<richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="vertical-align: top; font-weight: normal; font-style: normal; color: #ffffff">
+      несоответствие продукта или компонента спецификациям и ожиданиям.
+    </p>
+    <p style="vertical-align: top; font-weight: normal; font-style: normal; color: #ffffff">
+      Может быть обнаружен, как во время тестирования, так и после выпуска продукта.
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node ID="ID_1333736153" CREATED="1771432556003" MODIFIED="1771432614765"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Отказ (<span style="font-weight: bold;">Failure</span>)
+    </p>
+  </body>
+</html>
+</richcontent>
+<richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="vertical-align: top; font-weight: normal; font-style: normal; color: #ffffff">
+      Дефект, обнаруженный пользователем
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="Positive test" ID="ID_1177135143" CREATED="1771432756646" MODIFIED="1771432961522"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="vertical-align: top; font-weight: normal; font-style: normal; color: #ffffff">
+      проверка на правильных данных
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="Negative test" ID="ID_152057660" CREATED="1771432794126" MODIFIED="1771432854593"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      проверка на неправильных данных, реакция на некорректные действия
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="Test case" ID="ID_1733968803" CREATED="1771432801626" MODIFIED="1771432896156"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Выполняемый тест с чётко указанными входными данными, начальными условиями и ожидаемом результате
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node TEXT="Что должен знать Manual Tester" POSITION="top_or_left" ID="ID_1716873380" CREATED="1771432969510" MODIFIED="1771432975860">
+<node TEXT="Теория тестирования" ID="ID_1474467266" CREATED="1771432982401" MODIFIED="1771432990121"/>
+<node TEXT="Общая компьютерная грамотность" ID="ID_1570264246" CREATED="1771432992688" MODIFIED="1771433001552"/>
+<node TEXT="Инструменты тестировщика" ID="ID_1854643557" CREATED="1771433001950" MODIFIED="1771433009849"/>
+<node TEXT="Soft Skills" ID="ID_1930245269" CREATED="1771433012358" MODIFIED="1771433016670"/>
+<node TEXT="Навыки трудоустройства" ID="ID_1130786249" CREATED="1771433017018" MODIFIED="1771433028131"/>
+</node>
+</node>
+</map>
