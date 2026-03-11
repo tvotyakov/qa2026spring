1488466f1b4ab3093701e1b7c0cb204589b4086e
Timofey Votyakov <tvotyakov@gmail.com>
2026-03-11 13:22:47 +0100

    [CBQ-391] Add python lesson 6 homework (#5)

---
+++ Python/HomeWork06/task02.py
@@ -0,0 +1,8 @@
+power = lambda x, y: x ** y
+
+def run():
+    print('power(3, 1) =', power(3, 1))
+    print('power(3, 2) =', power(3, 2))
+    print('power(3, 3) =', power(3, 3))
+    print('power(3, 4) =', power(3, 4))
+    print('power(3, 0) =', power(3, 0))
