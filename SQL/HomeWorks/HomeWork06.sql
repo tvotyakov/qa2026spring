c671e4ef7afd591f1f0757c746120f0b46a0ec80
Timofey Votyakov <tvotyakov@gmail.com>
2026-03-17 20:19:20 +0100

    [CBQ-356] SQL lesson 6: Analysis open data dataset as a part of homework

--- SQL/HomeWorks/HomeWork06.sql
+++ SQL/HomeWorks/HomeWork06.sql
@@ -9,7 +9,97 @@
 -- https://catalog.data.gov/dataset
 -- https://data.world
 -- https://datasf.org/opendata/
+DROP DATABASE IF EXISTS data_gov;
+CREATE DATABASE IF NOT EXISTS data_gov;
 
+CREATE TABLE IF NOT EXISTS data_gov.air_quality
+(
+    `Unique ID` bigint not null
+  , `Indicator ID` bigint not null
+  , `Name` text
+  , `Measure` text
+  , `Measure Info` text
+  , `Geo Type Name` text
+  , `Geo Join ID` bigint
+  , `Geo Place Name` text
+  , `Time Period` text
+  , Start_Date text
+  , Data_Value text
+  , Message text
+);
+
+LOAD DATA LOCAL INFILE 'd:/learn/qa2026spring/SQL/Air_Quality.csv '
+  INTO TABLE data_gov.air_quality
+  FIELDS TERMINATED BY ','
+  ENCLOSED BY '"'
+  LINES TERMINATED BY '\n'
+  IGNORE 1 ROWS
+  ( `Unique ID`
+  , `Indicator ID`
+  , `Name`
+  , `Measure`
+  , `Measure Info`
+  , `Geo Type Name`
+  , `Geo Join ID`
+  , `Geo Place Name`
+  , `Time Period`
+  , Start_Date
+  , Data_Value
+  , Message);
+
+select count(*) from data_gov.air_quality;
+select * from data_gov.air_quality limit 100;
+
+-- Count measures by Name
+select Name, count(*)
+  from data_gov.air_quality
+group by Name;
+
+-- Count measures by type
+select Measure, count(*)
+  from data_gov.air_quality
+group by Measure;
+
+select *
+  from data_gov.air_quality
+  where Measure = 'Million miles';
+
+alter table data_gov.air_quality
+    rename column Start_Date to Start_Date_Str;
+
+alter table data_gov.air_quality
+    add column Start_Date date;
+
+select distinct Start_Date_Str
+  from data_gov.air_quality;
+
+update data_gov.air_quality
+  set  Start_Date = str_to_date(Start_Date_Str, '%m/%d/%Y');
+
+select distinct Start_Date_Str, Start_Date
+  from data_gov.air_quality;
+
+-- select measures started in the last decade
+select  *
+  from  data_gov.air_quality
+  where YEAR(Start_Date) >= YEAR(NOW()) - 10;
+
+-- select count of measures by Name and Measure
+select  Name, Measure, count(*)
+  from  data_gov.air_quality
+group by Name, Measure;
+
+-- select count of Fine Particles measures
+-- and avg data_value by Name and Time Period
+select  Name, Measure, `Time Period`, count(*), avg(Data_Value)
+  from data_gov.air_quality
+  where Name = 'FIne particles (PM 2.5)'
+group by Name, Measure, `Time Period`;
+
+-- select measures with messages
+select  *
+  from  data_gov.air_quality
+  where IFNULL(Message, '') != '';
 
 -- ---------------------------------------------------
 -- Homework - Part #3
